ccdbafe78a824ef3fa0ec91d3deb9f141afd499a
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2025-04-10 14:22:44 +0200

    Refactor Route History | Chat

--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/chat/send', [ChatMessageController::class, 'sendMessage']);
-Route::get('/chat/history', [ChatMessageController::class, 'getChatHistory']);
+Route::get('/chat/history/{sender_id}/{receiver_id}', [ChatMessageController::class, 'getChatHistory']);
 Route::post('/chat/mark-as-read/{messageId}', [ChatMessageController::class, 'markAsRead']);
